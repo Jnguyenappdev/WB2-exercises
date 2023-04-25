ea4df30309d7798c438806ec12e2ef9862215968
Julle Nguyen <jnguyen@appdev.yearup.org>
2023-04-25 10:34:11 -0400

    area_of_rectangle file

---
+++ MathScripts/area_of_rectangle.js
@@ -0,0 +1,9 @@
+var recLenght
+var recWidth
+
+// formula
+var recArea = recLenght * recWidth
+
+console.log(
+    "The area of the rectangle " + recArea
+)
--- MathScripts/net_worth.js
+++ MathScripts/net_worth.js
@@ -1,5 +1,8 @@
+
 var assets
 var debts 
+
+// formula
 var net_worth = assets - debts
 console.log(
     "Your net worth is " + net_worth
