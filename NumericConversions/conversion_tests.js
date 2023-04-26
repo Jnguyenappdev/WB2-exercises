3042b941a533a097d93e34d73c41edb2039f5ead
Julle Nguyen <jnguyen@appdev.yearup.org>
2023-04-26 09:15:50 -0400

    numeric conversion

---
+++ NumericConversions/conversion_tests.js
@@ -0,0 +1,21 @@
+// Description: This script tests various numeric
+// conversion techniques
+// Author: Jordan Q. Newprogrammer
+
+var a = " 101.1 ";
+var b = "55";
+var c = "402 Stevens";
+var d = "Number 5 ";
+
+console.log(
+    "a : " + a 
+)
+console.log(
+    "b : " + b 
+)
+console.log(
+    "c : " + c 
+)
+console.log(
+    "d : " + d 
+)
